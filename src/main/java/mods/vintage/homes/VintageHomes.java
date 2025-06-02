c4ad10a419c0f5746e36189f2510c230a3fc1084
CrossVas <cross.wanted.1.4@gmail.com>
2025-06-02 13:47:16 +0900

    added: save death position as warp point for easier (OP) access

--- src/main/java/mods/vintage/homes/VintageHomes.java
+++ src/main/java/mods/vintage/homes/VintageHomes.java
@@ -4,14 +4,18 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import mods.vintage.core.helpers.ConfigHelper;
+import mods.vintage.core.platform.lang.FormattedTranslator;
 import mods.vintage.core.platform.lang.ILangProvider;
 import mods.vintage.core.platform.lang.LangManager;
-import mods.vintage.homes.command.CommandDimensionTP;
-import mods.vintage.homes.command.CommandHome;
-import mods.vintage.homes.command.CommandSetHome;
-import mods.vintage.homes.command.CommandWarp;
+import mods.vintage.homes.command.*;
 import mods.vintage.homes.utils.PlayerTracker;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,6 +35,7 @@ public class VintageHomes implements ILangProvider {
         commandPermissionLevel = ConfigHelper.getInt(config, "general", "commandPermissionLevel", 0, 4, 0, "Minimum required permission level to use commands (0 = all players, 4 = server owner)");
         if (config.hasChanged()) config.save();
         LangManager.THIS.registerLangProvider(this);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Mod.ServerStarting
@@ -39,6 +44,25 @@ public class VintageHomes implements ILangProvider {
         e.registerServerCommand(new CommandSetHome());
         e.registerServerCommand(new CommandDimensionTP());
         e.registerServerCommand(new CommandWarp());
+    }
+
+    @ForgeSubscribe
+    public void onPlayerDeath(LivingDeathEvent e) {
+        Entity entity = e.entity;
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+            int[] data = {
+                    player.worldObj.provider.dimensionId,
+                    (int) player.posX,
+                    (int) player.posY,
+                    (int) player.posZ
+            };
+            String deathPointName = player.username + "_deathPoint";
+            VintageHomesWorldStorage storage = VintageHomesWorldStorage.get(MinecraftServer.getServer());
+            storage.warpsTag.setIntArray(deathPointName, data);
+            storage.markDirty();
+            player.sendChatToPlayer(FormattedTranslator.GREEN.format("message.command.warp.success.add", FormattedTranslator.AQUA.literal(deathPointName)));
+        }
     }
 
     @Override
